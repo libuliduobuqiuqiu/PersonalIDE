c23e14cc8142dbbbedce2e17ee0b05b10f0436b8
libuliduobuqiuqiu <mbb929833@gmail.com>
2024-12-11 14:22:56 +0800

    feat: update lazyvim plugins lua.

---
+++ lazyvim-lua/plugins/neogen.lua
@@ -0,0 +1,10 @@
+return {
+  "danymat/neogen",
+  config = true,
+  -- Uncomment next line if you want to follow only stable versions
+  -- version = "*"
+  cmd = "Neogen",
+  keys = {
+    { "<leader>gn", "<cmd>Neogen<CR>", desc = "neogen" },
+  },
+}
